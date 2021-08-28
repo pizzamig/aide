d1cd27618adf047bc628e998095634d047690530
Luca Pizzamiglio <pizzamig@FreeBSD.org>
2021-08-28 12:11:41 +0200

    Initial implementation of the weather API

--- weatherapi/src/main.rs
+++ weatherapi/src/main.rs
@@ -2,7 +2,6 @@ mod cli;
 mod weatherapi;
 
 use clap::Clap;
-
 const WEATHERAPI_PORT: u16 = 9091;
 
 #[async_std::main]
@@ -12,6 +11,8 @@ async fn main() -> tide::Result<()> {
 
     let mut app = tide::with_state(opt);
     app.at("/v1/current").get(current);
+    app.at("/v1/forecast").get(forecast);
+    app.at("/v1/hourrainforecast").get(hour_rain_forecast);
     let binded = format!("0.0.0.0:{}", WEATHERAPI_PORT);
     app.listen(binded).await?;
     Ok(())
@@ -35,6 +36,53 @@ async fn current(req: tide::Request<cli::Opt>) -> tide::Result<String> {
     let client = surf::Client::new();
     let mut res = client.get(forecast_url).send().await.unwrap();
     let resp_forecast: weatherapi::ForecastResponse = res.body_json().await.unwrap();
-    dbg!(resp_forecast);
-    Ok("".to_string())
+    //dbg!(&resp_forecast);
+    let result: aide_proto::v1::weather::CurrentWeather = From::from(resp_forecast);
+    Ok(serde_json::to_string(&result).unwrap())
+}
+
+async fn forecast(req: tide::Request<cli::Opt>) -> tide::Result<String> {
+    let location_str = req
+        .param("location")
+        .unwrap_or_else(|_| req.state().location.as_str());
+    let base_url = surf::Url::parse(weatherapi::WEATHERAPI_BASE_URL).unwrap();
+    let mut forecast_url = base_url.join("forecast.json").unwrap();
+    forecast_url
+        .query_pairs_mut()
+        .append_pair("days", "2")
+        .append_pair("aqi", "yes")
+        .append_pair("alerts", "yes");
+    forecast_url
+        .query_pairs_mut()
+        .append_pair("key", req.state().key.as_str())
+        .append_pair("q", location_str);
+    let client = surf::Client::new();
+    let mut res = client.get(forecast_url).send().await.unwrap();
+    let resp_forecast: weatherapi::ForecastResponse = res.body_json().await.unwrap();
+    //dbg!(&resp_forecast);
+    let result: aide_proto::v1::weather::Forecast = From::from(resp_forecast);
+    Ok(serde_json::to_string(&result).unwrap())
+}
+
+async fn hour_rain_forecast(req: tide::Request<cli::Opt>) -> tide::Result<String> {
+    let location_str = req
+        .param("location")
+        .unwrap_or_else(|_| req.state().location.as_str());
+    let base_url = surf::Url::parse(weatherapi::WEATHERAPI_BASE_URL).unwrap();
+    let mut forecast_url = base_url.join("forecast.json").unwrap();
+    forecast_url
+        .query_pairs_mut()
+        .append_pair("days", "2")
+        .append_pair("aqi", "yes")
+        .append_pair("alerts", "yes");
+    forecast_url
+        .query_pairs_mut()
+        .append_pair("key", req.state().key.as_str())
+        .append_pair("q", location_str);
+    let client = surf::Client::new();
+    let mut res = client.get(forecast_url).send().await.unwrap();
+    let resp_forecast: weatherapi::ForecastResponse = res.body_json().await.unwrap();
+    //dbg!(&resp_forecast);
+    let result: Vec<aide_proto::v1::weather::HourRainForecast> = From::from(resp_forecast);
+    Ok(serde_json::to_string(&result).unwrap())
 }
