f6f8c03d006c0a5b081697c4c9ae6235db52b5aa
Luca Pizzamiglio <pizzamig@FreeBSD.org>
2021-12-27 22:16:53 +0100

    Introducte the CliCommonOpt struct
    host_addr has been converted to a String to support names, not only ip
    addresses

--- aide-common/src/cli.rs
+++ aide-common/src/cli.rs
@@ -15,3 +15,23 @@ pub struct CommonOpt {
     /// Listening TCP port of the server (default: 80)
     pub port: u16,
 }
+
+#[derive(Parser, Clone, Debug)]
+pub struct CliCommonOpt {
+    #[clap(
+        name = "server",
+        long = "Server",
+        short = 'S',
+        default_value = "127.0.0.1"
+    )]
+    /// Set the listening IP address (default: 127.0.0.1)
+    pub host_addr: String,
+
+    #[clap(short = 'p', long, default_value_t = 80)]
+    /// Listening TCP port of the server (default: 80)
+    pub port: u16,
+
+    #[clap(long = "no-tls")]
+    /// Force http to connect to the server (default: false)
+    pub notls: bool,
+}
