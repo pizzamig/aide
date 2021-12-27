cc4b22fb940f0b4f9f914b27180c387df3a74999
Luca Pizzamiglio <pizzamig@FreeBSD.org>
2021-12-27 22:41:01 +0100

    Restore an erroneously removed lined

--- habitica/src/main.rs
+++ habitica/src/main.rs
@@ -25,6 +25,7 @@ async fn main() -> Result<(), anyhow::Error> {
         .unwrap_or_else(|_| panic!("The env var {} is missing", HABITICA_USER_ENV_VAR));
     let client_id = std::env::var(CLIENT_ID_ENV_VAR)
         .unwrap_or_else(|_| panic!("the env var {} is missing", CLIENT_ID_ENV_VAR));
+    let state = HabiticaState {
         key,
         user,
         client_id,
